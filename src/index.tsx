3ee4d1fdfd912cf39c4cb1d217f0fb441ddd34db
p-le <p-le@dimage.co.jp>
2017-08-27 07:35:29 +0900

    remove styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import { App } from './containers/App';
-import { theme } from './utils/theme';
 
 ReactDOM.render(
-    <ThemeProvider theme={theme}>
-        <App />
-    </ThemeProvider>,
+    <App />,
     document.getElementById('app')
 );
--- src/utils/bind.ts
+++
@@ -1,3 +0,0 @@
-export function bind<T>(fns: string[], context: T) {
-    fns.map((fn) => (context as any)[fn] = (context as any)[fn].bind(context));
-}
